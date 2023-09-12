8bbbfffd2f9ea9f98dee8d72b055dfc8db7f80d7
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2023-09-12 15:04:26 -0300

    exerc1 feito

--- exerc1.py
+++ exerc1.py
@@ -1,5 +1,6 @@
 from EstruturasSimplificadas import *
 
+
 def exerc1(nome_arq = "in1.txt"):
     # Leia a entrada de uma linha de cada vez e, em seguida, escreva as 
     # linhas em ordem inversa, de modo que a última linha de entrada 
@@ -9,20 +10,36 @@ def exerc1(nome_arq = "in1.txt"):
 
     
     try :
-        arq_in = open(nome_arq, "r")
+        arq_in = open(nome_arq, "r", encoding="utf-8")
     except IOError:
         print("Erro ao abrir arquivo de entrada.")
         return
     
-    # Escreva aqui sua resposta para o exercício 1. Não esqueça de usar a função strip() 
-    # para remover os espaços em branco no início e no fim da string.
-    # ATENÇÃO: não use a função readlines() para ler o arquivo de entrada.
-    # Você deve usar a estrutura simplificada Pilha, Fila, Deque, Sset, Uset ou FilaPrioridade
+    
+    
+    
+    
+    
+    deque = Deque()
+    sizeFile = len(arq_in.readlines())
+    size = 0
 
+    
+    arq_in.seek(0,0)
+    while(size < sizeFile):
+        deque.add_last(arq_in.readline())
+        size += 1
 
-    # Fim da sua resposta para o exercício 1.
-    # fechar arquivo de entrada
+    while(size >0):
+        print(deque.remove_last())
+        size -= 1
+    
+    
     arq_in.close()
+
+    
+    
+    
 
 if __name__ == "__main__":
     exerc1()
